971074d78b4946db81504c170ccab90bdb9ed32a
rafs42 <rafs42@gmail.com>
2020-06-29 20:35:31 +0300

    test2

--- src/main/java/TestPackage1/App.java
+++ src/main/java/TestPackage1/App.java
@@ -9,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World2" );
+        System.out.println( "Hello World3" );
         System.out.println(DigestUtils.sha256("helloworld"));
     }
 }
