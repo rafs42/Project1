cbc15e10061aba176a6c8ce52d4b479e96a6bb3f
rafs42 <rafs42@gmail.com>
2020-06-29 20:34:38 +0300

    Initial commit

---
+++ src/main/java/TestPackage1/App.java
@@ -0,0 +1,15 @@
+package TestPackage1;
+
+import org.apache.commons.codec.digest.DigestUtils;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World2" );
+        System.out.println(DigestUtils.sha256("helloworld"));
+    }
+}
